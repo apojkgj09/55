2ffbd1ca54b4b69afec8a79f986657b1ac8a15a2
المايسترو <128506664+apojkgj09@users.noreply.github.com>
2023-06-17 06:24:21 +0300

    Update songs.py

--- ShizukaXMusic/plugins/tools/songs.py
+++ ShizukaXMusic/plugins/tools/songs.py
@@ -23,7 +23,7 @@ ydl_opts = {
 
 
 @app.on_message(
-    filters.command(["تحميل","يوتيوب","بحث"],"") & ~filters.edited)
+    filters.command(["تحميل","يوت","بحث"],"") & ~filters.edited)
 def song(_, message):
     query = " ".join(message.command[1:])
     m = message.reply("- **ابشر جاري البحث ..**")
@@ -102,7 +102,7 @@ def song232(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**- Ch** [تحديثات لندا ♪](t.me/FH_KP)"
+        rep = f"**𝙘𝙝𝙖𝙣𝙣𝙚𝙡** [ 𝙨𝙤𝙪𝙧𝙘𝙚 𝙡𝙞𝙣𝙙𝙖](t.me/FH_KP)"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
